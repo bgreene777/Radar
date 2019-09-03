de4bd5f571ddf7dcd2e6fb7ffce5076646d25c52
Brian Greene <bgreene777@gmail.com>
2019-09-03 12:51:37 -0500

    q2

--- SP1/sinsamp.m
+++ SP1/sinsamp.m
@@ -1,33 +1,36 @@
-%
+% Brian R. Greene
 % sinsamp.m
 %
 % This program will sample a sinusoidal signal
 % and plot the result.
 %
-clear all;
-samptime=1/1000;
-freq=10;
-for ii=1:1000
-    time(ii)=(ii-1)*samptime;
-    x(ii)=cos(2*pi*freq*time(ii));
-end
+clc
+clear
+close all
+
+% freq = 20 Hz, samp = 100 Hz
+samptime=1/100; % s
+freq=20; % Hz
+time=[0:99]*samptime;
+x=cos(2*pi*freq*time);
 subplot(2,1,1);
 plot(time,x);
-title('Sampled Sinusoid with loop');
+title('Sinusoid sampled at 100 Hz');
 xlabel('Time (sec)');
 ylabel('x(t)');
 grid on;
+
 %
-% accomplished above but w/o loop
+% freq = 20 Hz, samp = 20 Hz
 %
 clear all;
-samptime=1/1000;
-freq=10;
-time=[0:999]*samptime;
+samptime=1/20;
+freq=20;
+time=[0:99]*samptime;
 x=cos(2*pi*freq*time);
 subplot(2,1,2);
 plot(time,x);
-title('Sampled Sinusoid w/o loop');
+title('Sinusoid sampled at 20 Hz');
 xlabel('Time (sec)');
 ylabel('x(t)');
 grid on;
