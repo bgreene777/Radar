e887a953525765573ceb84d9466474d1db367b94
Brian Greene <bgreene777@gmail.com>
2019-09-04 15:24:02 -0500

    done with q7

--- SP1/polyrefractivity.m
+++ SP1/polyrefractivity.m
@@ -22,28 +22,33 @@ r = data(:, 6);
 r = r/1000;
 pw = p .* (r./(0.622 + r));
 N = (77.6./(t + 273)) .* (p + 4810*(pw./(t+273)));
-
-%% Calculate gradient
-iuse = find(z < 2000);
-dNdz = nan(length(iuse)-1, 1);
-znew = nan(length(iuse)-1, 1);
-
-for i = 2:length(iuse)
-    dN = N(i) - N(i-1);
-    dz = z(i) - z(i-1);
-    dNdz(i-1) = dN / dz;
-    znew(i-1) = (z(i) + z(i-1)) / 2;
-end
+n = (N * 1e-6) + 1;
 
 %% Fit polynomial
-p = polyfit(znew, dNdz, 1);
+iuse = find(z < 2000);
+p = polyfit(z(iuse), n(iuse), 1);
 x = 0:1:2000;
 fit = p(1)*x + p(2);
+
+%% Find ae
+% slope = dN/dz = p(1)
+a = 6374e3;
+dNdz = p(1);
+ae = 1/((1/a) + dNdz);
+disp('ae = ');
+disp(ae);
 
 %% Plot
 f1 = figure(1);
 f1.Position = [375,492,600,600];
 
 hold on
-scatter(dNdz, znew)
+scatter(n(iuse), z(iuse))
 plot(fit, x)
+title('Refractivity n vs. Altitude with polyfit')
+xlabel('Refractivity n [-]')
+ylabel('Altitude [m]')
+grid on
+
+% save
+print(f1, 'Q7', '-dpng', '-r300')
